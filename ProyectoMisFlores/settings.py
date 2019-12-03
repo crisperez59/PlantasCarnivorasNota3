2a5a0ccc6659423ad66f9f3c4c532514dba9dbf0
luciano1997 <crisazulldea@gmail.com>
2019-12-03 10:06:28 -0300

    Algunso ajustes

--- ProyectoMisFlores/settings.py
+++ ProyectoMisFlores/settings.py
@@ -112,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'es-cl'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Santiago'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
